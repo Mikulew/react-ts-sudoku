545a04957640bf3a1f167b60c7e6a2a18c3c2e86
Mikulew <mikulew@gmail.com>
2021-08-31 17:03:58 +0200

    Remove unnecessary code

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import reportWebVitals from './core/report-web-vitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>Hello World!</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
